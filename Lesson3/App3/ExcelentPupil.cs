552f38252cc27f9a1df70ea366230a1f55b9b0be
TieR <tiervg@gmail.com>
2017-06-15 12:20:22 +0300

    Add app3

---
+++ Lesson3/App3/ExcelentPupil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App3
+{
+    class ExcelentPupil:Pupil
+    {
+        protected override void Read()
+        {
+            Console.WriteLine("Excelent pupil has brilliant read skill");
+        }
+
+        protected override void Study()
+        {
+            Console.WriteLine("Excelent pupil well and quick learn");
+        }
+
+        protected override void Relax()
+        {
+            Console.WriteLine("Excelent pupil don't have time to relax");
+        }
+
+        protected override void Write()
+        {
+            Console.WriteLine("Exelent pupil ");
+        }
+
+        public override void Show()
+        {
+            base.Show();
+        }
+    }
+}
---
+++ Lesson3/App3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//x Создать класс, представляющий учебный класс ClassRoom.
+//x Создайте класс ученик Pupil.В теле класса создайте методы void Study(), void Read(), void Write(), void Relax().  
+//x Создайте 3 производных класса ExcelentPupil, GoodPupil, BadPupil от класса базового класса Pupil и переопределите каждый из методов, в зависимости от успеваемости ученика.
+//x Конструктор класса ClassRoom принимает аргументы типа Pupil, класс должен состоять из 4 учеников.
+//x Предусмотрите возможность того, что пользователь может передать 2 или 3 аргумента.
+//x Выведите информацию о том, как все ученики экземпляра класса ClassRoom умеют учиться, читать, писать, отдыхать.
+
+namespace App3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExcelentPupil excelent = new ExcelentPupil();
+            GoodPupil good = new GoodPupil();
+            BadPupil bad = new BadPupil();
+            ClassRoom classRoom = new ClassRoom(excelent,good,bad);
+            classRoom.Show();
+        }
+    }
+}
