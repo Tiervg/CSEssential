a81167b8af99411553f98f57302ac2316cff86d2
TieR <tiervg@gmail.com>
2017-06-20 13:19:32 +0300

    Finish Lesson5

---
+++ Lesson5/App3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//x Создать класс Article, содержащий следующие закрытые поля: 
+//x • название товара; 
+//todo • название магазина, в котором продается товар; 
+//todo • стоимость товара в гривнах.
+//todo Создать класс Store, содержащий закрытый массив элементов типа Article.  
+//todo Обеспечить следующие возможности: 
+//todo • вывод информации о товаре по номеру с помощью индекса; 
+//todo • вывод на экран информации о товаре, название которого введено с клавиатуры, если таких товаров нет, выдать соответствующее сообщение; 
+//Написать программу, вывода на экран информацию о товаре.
+
+namespace App3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Store store = new Store();
+            Console.WriteLine(store["Router"]);
+            Console.WriteLine(store[1]);
+            Console.WriteLine(store["Videocard"]);
+            Console.WriteLine(store[5]);
+        }
+    }
+}
---
+++ Lesson5/App3/Store.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App3
+{
+    class Store
+    {
+        Article[] articles = { new Article("Videocard AMD RX490", "Enter", 450.90), new Article("CPU Intel CoreI7 6700k", "Server", 345.5), new Article("Router", "D-Link", 25) };
+
+        public string this[string index]
+        {
+            get
+            {
+                for (int i = 0; i < articles.Length; i++)
+                {
+                    if (index == articles[i].ProductName)
+                    {
+                        return articles[i].ProductName + ",\0" + articles[i].ShopName + ",\0" + articles[i].Price;
+                    }
+                }
+                return string.Format("There is no article found");
+            }
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index>=0&&index<=articles.Length)
+                {
+                    for (int i = 0; i < articles.Length; i++)
+                    {
+                        if (index == i)
+                        {
+                            return articles[i].ProductName + ",\0" + articles[i].ShopName + ",\0" + articles[i].Price;
+                        }
+                    }
+                }
+                return string.Format("Attempt to access array limits");
+            }
+        }
+    }
+}
