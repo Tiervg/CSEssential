911cda82c8471cea97090482440a29b5ec97cd18
Tiervg <Tiervg@gmail.com>
2017-06-23 11:45:20 +0300

    les6,app2

---
+++ Lesson6/App2,App3/Book/Author.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2_App3
+{
+    class Author
+    {
+        string name;
+        string lastname;
+
+        public string Name { get { return name; } }
+        public string Lastname { get { return lastname; } }
+
+        public Author(string name,string lastname)
+        {
+            this.name = name;
+            this.lastname = lastname;
+        }
+       
+    }
+}
