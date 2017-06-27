c143ecf078a8c9eefb8e9e40236dd44f6a5638f5
TieR <tiervg@gmail.com>
2017-06-28 00:49:23 +0300

    I want sleep

---
+++ Lesson7/App1/Notebook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    struct Notebook
+    {
+        string model, manufacture;
+        double price;
+
+        public Notebook(string manufacture, string model, double price)
+        {
+            this.manufacture = manufacture;
+            this.model = model;
+            this.price = price;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Notebook: {0} {1} cost: {2}\u0024", manufacture, model, price);
+        }
+    }
+}
---
+++ Lesson7/App1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//x Создайте структуру с именем - Notebook.
+//x Поля структуры: модель, производитель, цена.
+//x В структуре должен быть реализован конструктор для инициализации полей и метод для вывода содержимого полей на экран.
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook noteBook = new Notebook("Acer", "Aspire 5750g", 554.60);
+            noteBook.Show();
+        }
+    }
+}
