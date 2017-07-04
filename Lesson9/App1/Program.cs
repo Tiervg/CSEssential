690889314b993fe000654b3ce9edb8da1acaa894
TieR <tiervg@gmail.com>
2017-07-04 13:24:24 +0300

    Lesson 9 finish

---
+++ Lesson9/App1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Создайте анонимный метод, который принимает в качестве параметров три целочисленных аргумента и возвращает среднее арифметическое этих аргументов.
+
+namespace App1
+{
+
+    public delegate int AverageMean(int a, int b, int c);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // initialize variables by random values
+            Random rand = new Random();
+            int first = rand.Next(1, 10), second = rand.Next(1, 10), third = rand.Next(1, 10);
+
+            // Anonimized method
+            AverageMean mean = delegate (int a, int b, int c) { return (a + b + c) / 3; };
+
+            //Show result
+            Console.WriteLine("({0} + {1} + {2}) / 3 = {3}",first,second,third,mean(first, second, third));
+        }
+    }
+}
---
+++ Lesson9/App3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Создайте анонимный метод, который принимает в качестве аргумента массив делегатов и возвращает среднее арифметическое возвращаемых значений методов 
+// сообщенных с делегатами в массиве.Методы, сообщенные с делегатами из массива, возвращают случайное значение типа int. 
+namespace App3
+{
+    delegate int AverageMean(Delegate[] arg);
+    delegate int RandValue();
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res =0;
+            Random rand = new Random();
+            // create delegates array
+            RandValue[] delegatesArray = new RandValue[3];
+            //every delegate in array have lambda expresion that return random value
+            for (int i = 0; i < delegatesArray.Length; i++)
+            {
+                delegatesArray[i] = () => rand.Next(10, 100);
+            }
+
+            AverageMean mean = (array) =>
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                   res += (int) array[i].DynamicInvoke();
+                }
+                return res / array.Length;
+            };
+            Console.WriteLine(mean(delegatesArray));
+        }
+    }
+}
