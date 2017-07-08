8177ac4b9ebe9027a749ac1cf4579e038de22891
Tier <tiervg@gmail.com>
2017-07-08 17:17:14 +0300

    lesson 10 app3

---
+++ Lesson10/App3/MyDictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App3
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keyArray = new TKey[4];
+        TValue[] valueArray = new TValue[4];
+
+        public void Add(TKey key, TValue value)
+        {
+            for (int i = 0; i < keyArray.Length; i++)
+            {
+                for (int j = i; j < valueArray.Length; j++)
+                {
+
+                    if (keyArray[i]==null&&valueArray[i]==null)
+                    {
+                        keyArray[i] = key;
+                        valueArray[j] = value;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public T
+
+    }
+}
---
+++ Lesson10/App3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//todo Создайте класс MyDictionary<TKey, TValue>.
+//todo Реализуйте в простейшем приближении возможность использования его экземпляра аналогично экземпляру класса Dictionary(Урок 6 пример 5). 
+//todo Минимально требуемый интерфейс взаимодействия с экземпляром, должен включать метод добавления пар элементов, индексатор для получения значения элемента по указанному индексу
+//todo свойство только для чтения для получения общего количества пар элементов
+namespace App3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> dictionary = new MyDictionary<string, string>();
+            dictionary.Add("Hello", "world");
+        }
+    }
+}
