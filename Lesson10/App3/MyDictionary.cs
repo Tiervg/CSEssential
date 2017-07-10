4e3385f55e01253085e48d271a54db150b6697af
Tier <tiervg@gmail.com>
2017-07-10 14:49:12 +0300

    finish app 3

--- Lesson10/App3/MyDictionary.cs
+++ Lesson10/App3/MyDictionary.cs
@@ -8,27 +8,91 @@ namespace App3
 {
     class MyDictionary<TKey, TValue>
     {
-        TKey[] keyArray = new TKey[4];
-        TValue[] valueArray = new TValue[4];
+        int length;
+        TKey[] keyArray;
+        TValue[] valueArray;
+
+        public MyDictionary(int length)
+        {
+            keyArray = new TKey[length];
+            valueArray = new TValue[length];
+            this.length = length; 
+        }
 
         public void Add(TKey key, TValue value)
         {
+            bool flag = false;
             for (int i = 0; i < keyArray.Length; i++)
             {
-                for (int j = i; j < valueArray.Length; j++)
+                for (int j = 0; j < valueArray.Length; j++)
                 {
 
-                    if (keyArray[i]==null&&valueArray[i]==null)
+                    if (keyArray[i] == null && valueArray[j] == null)
                     {
                         keyArray[i] = key;
                         valueArray[j] = value;
+                        flag = true;
                         break;
                     }
+                }
+                if (flag)
+                {
+                    break;
                 }
             }
         }
 
-        public T
+        public TValue this[TKey index]
+        {
+            get
+            {
+                TValue temp = default(TValue);
+                int z = 0;
+                for (int i = 0; i < keyArray.Length; i++)
+                {
+                    if (index.Equals(keyArray[i]))
+                    {
+                        z = i;
+                    }
+                }
+                for (int i = 0; i < valueArray.Length; i++)
+                {
+                    if (z == i)
+                    {
+                        temp = valueArray[i];
+                    }
+                }
+                return temp;
+            }
+        }
+
+        //public TKey this[TValue index]
+        //{
+        //    get
+        //    {
+        //        TKey temp = default(TKey);
+        //        int z = 0;
+        //        for (int i = 0; i < valueArray.Length; i++)
+        //        {
+        //            if (index.Equals(keyArray[i]))
+        //            {
+        //                z = i;
+        //            }
+        //        }
+
+        //        for (int i = 0; i < keyArray.Length; i++)
+        //        {
+        //            if (z == i)
+        //            {
+        //                temp = keyArray[i];
+        //            }
+        //        }
+        //        return temp;
+        //    }
+        //}
+
+        public int Lenght { get { return length; } }
+
 
     }
 }
--- Lesson10/App3/Program.cs
+++ Lesson10/App3/Program.cs
@@ -4,18 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//todo Создайте класс MyDictionary<TKey, TValue>.
-//todo Реализуйте в простейшем приближении возможность использования его экземпляра аналогично экземпляру класса Dictionary(Урок 6 пример 5). 
-//todo Минимально требуемый интерфейс взаимодействия с экземпляром, должен включать метод добавления пар элементов, индексатор для получения значения элемента по указанному индексу
-//todo свойство только для чтения для получения общего количества пар элементов
+//x Создайте класс MyDictionary<TKey, TValue>.
+//x Реализуйте в простейшем приближении возможность использования его экземпляра аналогично экземпляру класса Dictionary(Урок 6 пример 5). 
+//x Минимально требуемый интерфейс взаимодействия с экземпляром, должен включать метод добавления пар элементов, индексатор для получения значения элемента по указанному индексу
+//x свойство только для чтения для получения общего количества пар элементов
 namespace App3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            MyDictionary<string, string> dictionary = new MyDictionary<string, string>();
-            dictionary.Add("Hello", "world");
+            MyDictionary<string, string> dictionary = new MyDictionary<string, string>(5);
+            dictionary.Add("мир", "world");
+            dictionary.Add("удача", "luck");
+            dictionary.Add("крыша", "roof");
+            Console.WriteLine(dictionary["крыша"]);
+            Console.WriteLine(dictionary.Lenght);
+
+
         }
     }
 }
