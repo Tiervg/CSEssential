306f5396dd322f6cef09a3f9254fb15cd49d279b
Tier <tiervg@gmail.com>
2017-07-13 19:19:14 +0300

    lesson 11

---
+++ Lesson11/App1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// В коллекцию  ArrayList, через вызов метода Add добавьте элементы структурного и ссылочного типа, переберите данную коллекцию с помощью, цикла for – С какой проблемой вы столкнулись?
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList
+            {
+                10,
+                "Hello",
+                20,
+                "Eizo",
+                30,
+                "Philips",
+                40,
+                "Defender"
+            };
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+    }
+}
---
+++ Lesson11/App3/Dictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App3
+{
+    class Dictionary<Tkey, TValue>
+    {
+
+        class Key<T> where T : Tkey,new()
+        {
+            static int count = 4;
+            T[] array = new T[count];
+
+            public void Add(T key)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i]==null)
+                    {
+                        array[i] = key;
+                    }
+                }
+            }
+            public T this[T index]
+            {
+                get
+                {
+                    T temp = new T();
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        if (array[i]!= null)
+                        {
+                            temp = array[i];
+                        }
+                    }
+                    return temp;
+                }
+            }
+
+        }
+
+        class Value<T> where T : TValue
+        {
+            static int count = 4;
+            T[] array =new  T[count];
+
+
+        }
+    }
+}
