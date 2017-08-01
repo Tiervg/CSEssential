3431abe5f73806c3c8521ef8f4ca71d4d1cecbb9
TieR <tiervg@gmail.com>
2017-08-01 13:41:33 +0300

    App1Lesson12

---
+++ Lesson12/App1/Presenter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// Presenter
+
+namespace MVP
+{
+    class Presenter
+    {
+        Model model = null;
+        MainWindow mainWindow = null;
+
+        event EventHandler myEvent
+        {
+            add { mainWindow.myEvent += value; }
+            remove { mainWindow.myEvent -= value; }
+        }
+
+        public Presenter(MainWindow mainWindow)
+        {
+            this.model = new Model();
+            this.mainWindow = mainWindow;
+            myEvent += mainWindow_myEvent;
+        }
+        
+
+
+        void mainWindow_myEvent(object sender, System.EventArgs e)
+        {
+            string variable = model.Logic(this.mainWindow.textBox1.Text);
+
+            this.mainWindow.textBox1.Text = variable;
+        }
+    }
+}
